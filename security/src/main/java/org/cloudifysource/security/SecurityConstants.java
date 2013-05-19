f2eb3153e3206ffb57bd0d2e4337c8207f0718e9
noak <noak@gigaspaces.com>
2013-05-19 15:50:38 +0300

    Checkstyle.

--- security/src/main/java/org/cloudifysource/security/SecurityConstants.java
+++ security/src/main/java/org/cloudifysource/security/SecurityConstants.java
@@ -26,13 +26,44 @@ public final class SecurityConstants {
 		// private constructor to prevent initialization.
 	}
 	
+	/**
+	 * Spring security xml file.
+	 */
 	public static final String SECURITY_FILE_NAME = "spring-security.xml";
+	
+	/**
+	 * The keystore file name.
+	 */
 	public static final String KEYSTORE_FILE_NAME = "keystore";
+	
+	/**
+	 * The environment variable holding the Spring security configuration file path.
+	 */
 	public static final String SPRING_SECURITY_CONFIG_FILE_ENV_VAR = "SPRING_SECURITY_CONFIG_FILE";
+	
+	/**
+	 * The environment variable holding the security keystore file path.
+	 */
 	public static final String KEYSTORE_FILE_ENV_VAR = "KEYSTORE_FILE";
+	
+	/**
+	 * The environment variable holding the security keystore password.
+	 */
 	public static final String KEYSTORE_PASSWORD_ENV_VAR = "KEYSTORE_KEY";
+	
+	/**
+	 * The environment variable holding Spring's active profiles (comma separated).
+	 */
 	public static final String SPRING_ACTIVE_PROFILE_ENV_VAR = "SPRING_PROFILES_ACTIVE";
+	
+	/**
+	 * The Spring profile used on a non-secured system.
+	 */
 	public static final String SPRING_PROFILE_NON_SECURE = "nonsecure";
+	
+	/**
+	 * The Spring profile used on a secured system.
+	 */
 	public static final String SPRING_PROFILE_SECURE = "secure";
 
 }

88a3b71d168c6f84b6e98d6e10fe0f9c283b9132
noak <noak@gigaspaces.com>
2013-05-23 10:57:59 +0300

    removed unused imports.

--- CLI/src/main/java/org/cloudifysource/shell/commands/InstallService.java
+++ CLI/src/main/java/org/cloudifysource/shell/commands/InstallService.java
@@ -13,8 +13,6 @@
 package org.cloudifysource.shell.commands;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;

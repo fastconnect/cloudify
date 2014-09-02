76f076c5e6d287a499ab9857b370bf764dd5c8e8
Nouh Bouanane <nouh.bouanane@fastconnect.fr>
2014-09-02 16:22:24 +0200

    initial class creation (networks,storage), using  gsVersion>9.7.1-SNAPSHOT

---
+++ esc/src/main/java/org/cloudifysource/esc/driver/provisioning/azure/MicrosoftAzureNetworkDriver.java
@@ -0,0 +1,11 @@
+package org.cloudifysource.esc.driver.provisioning.azure;
+
+/**
+ * Class that manages networks on azure cloud
+ * 
+ * @author fastconnect
+ *
+ */
+public class MicrosoftAzureNetworkDriver {
+
+}
---
+++ esc/src/main/java/org/cloudifysource/esc/driver/provisioning/azure/MicrosoftAzureStorageDriver.java
@@ -0,0 +1,11 @@
+package org.cloudifysource.esc.driver.provisioning.azure;
+
+/**
+ * Class that manages storages on azure cloud
+ * 
+ * @author fastconnect
+ *
+ */
+public class MicrosoftAzureStorageDriver {
+
+}
